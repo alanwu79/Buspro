54355532ce6e7d95f87d17be13e912b0d1b9434d
Kyle <mg148917@gmail.com>
2019-10-31 09:05:07 +0800

    sockServicce

---
+++ phyDev/phyDev.go
@@ -0,0 +1,136 @@
+package phyDev
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"strconv"
+	"sync"
+
+	//"github.com/goburrow/serial"
+	"github.com/tarm/serial"
+	"io"
+	"log"
+)
+
+var Wg sync.WaitGroup
+
+func ByteToHex(data []byte) string {
+	buffer := new(bytes.Buffer)
+	for _, b := range data {
+
+		s := strconv.FormatInt(int64(b&0xff), 16)
+		if len(s) == 1 {
+			buffer.WriteString("0")
+		}
+		buffer.WriteString(s)
+	}
+
+	return buffer.String()
+}
+
+func bytesToIntU(b []byte) (int, error) {
+	if len(b) == 3 {
+		b = append([]byte{0},b...)
+	}
+	bytesBuffer := bytes.NewBuffer(b)
+	switch len(b) {
+	case 1:
+		var tmp uint8
+		err := binary.Read(bytesBuffer, binary.BigEndian, &tmp)
+		return int(tmp), err
+	case 2:
+		var tmp uint16
+		err := binary.Read(bytesBuffer, binary.BigEndian, &tmp)
+		return int(tmp), err
+	case 4:
+		var tmp uint32
+		err := binary.Read(bytesBuffer, binary.BigEndian, &tmp)
+		return int(tmp), err
+	default:
+		return 0,fmt.Errorf("%s", "BytesToInt bytes lenth is invaild!")
+	}
+}
+
+func WriteSerial(port *serial.Port, writechan <-chan []byte) {
+
+	for {
+		log.Println("write")
+
+		command := <-writechan
+		log.Println(command)
+		n, err := port.Write([]byte(command))
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf(string(n))
+	}
+}
+
+
+
+func ReadSerialPort( port *serial.Port)  {
+	log.Println("read")
+
+	readBuf := make([]byte, 256)
+	var sumBuf = []byte{}
+	var renewBuf = []byte{}
+	sendBuf := make([]byte, 256)
+	var commandSize = 0
+	bufSize := 4096
+	headSize := 2
+
+	for {
+		num, err := port.Read(readBuf)
+		if err == io.EOF {
+			fmt.Println("EOF:", num)
+			break
+		}
+		if err != nil {
+			fmt.Errorf("cannnot open serial port: serialPort: %v, Error: %v", port, err)
+		}
+		if num > 0 {
+			log.Println("num:",num)
+			//log.Println("readBuf:", readBuf ,"len:" , len(readBuf))
+			for index := range readBuf[:num] {
+
+				sumBuf = append(sumBuf, readBuf[index])
+
+				log.Println("index:", index,"readBuf[index]:", readBuf[index] ,"sumBuf:" , sumBuf)
+			}
+
+			if(ByteToHex(sumBuf[2:3])!="00") {
+				commandSize,err = bytesToIntU(sumBuf[2:3])
+				if err != nil {
+					log.Println("bytesToIntU Fail:")
+					break
+				}
+
+				bufSize = commandSize + headSize
+
+				log.Println("bufSize:" , bufSize)
+			}
+
+			for len(sumBuf) >= bufSize {
+				sendBuf = sumBuf[:bufSize]
+
+				// Truncate sumBuf by Size
+				log.Println("sumBuf: ", sumBuf, "len(sumBuf): ", len(sumBuf))
+				log.Println("sendBuf: ", sendBuf, "len(sendBuf): ", len(sendBuf))
+				renewBuf = []byte{}
+				for index := bufSize; index < len(sumBuf); index++ {
+					renewBuf = append(renewBuf, sumBuf[index])
+					log.Println("renewBuf[0]:",renewBuf[0])
+					if(renewBuf[0])!=byte(170) {
+						renewBuf = renewBuf[1:]
+					}
+				}
+				log.Println("renewBuf: ",renewBuf)
+				sumBuf = renewBuf
+				log.Println("renewed sumBuf: ",sumBuf, "len:",len(sumBuf) ,"Size:",bufSize )
+			}
+			log.Println("break")
+		}
+	}
+	Wg.Done()
+}
---
+++ sockServicce/sockServicce.go
@@ -0,0 +1,97 @@
+package sockServicce
+
+import (
+	"bytes"
+	"encoding/binary"
+	"net"
+	_"syscall"
+	"os"
+	_"encoding/json"
+	"fmt"
+
+	)
+
+var unixCon *net.UnixConn
+const unixSocketFile = "/tmp/testunix.sock"
+
+func ConnectToUnixSocket() bool {
+	addr, err := net.ResolveUnixAddr("unix", unixSocketFile)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	unixCon, err = net.DialUnix("unix", nil, addr)
+	if err != nil {
+		fmt.Println("net.DialUnix error: ", err.Error())
+		unixCon = nil
+		return false
+	}
+	return true
+}
+
+func CloseUnixSocket() {
+	if unixCon != nil {
+		unixCon.Close()
+	}
+}
+
+func SendRequest(conn *net.UnixConn, data []byte) {
+	buf := new(bytes.Buffer)
+	msglen := uint32(len(data))
+
+	binary.Write(buf, binary.BigEndian, &msglen)
+	data = append(buf.Bytes(), data...)
+
+	conn.Write(data)
+}
+
+//读结果
+func ReadUnix(listener *net.UnixConn) {
+	for {
+		buf :=make([]byte, 1400)
+		size, remote, err := listener.ReadFromUnix(buf)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println("recv:", string(buf[:size]), " from ", remote.String())
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+		os.Exit(1)
+	}
+}
+
+func RunUnix() {
+
+	addr, err := net.ResolveUnixAddr("unix", unixSocketFile)
+	checkError(err)
+	unixCon, err = net.DialUnix("unix", nil, addr)
+	//defer unixCon.Close()
+
+	wsocketbuf := []byte{0xAA, 0xAA, 0x10, 0x01, 0xFA, 0x23, 0x9C, 0x00, 0x31, 0x01, 0x05, 0x01, 0x64, 0x00, 0x00, 0x01,
+		0x26, 0xA7}
+	//send to its subs
+	go ReadUnix(unixCon)
+	go SendRequest(unixCon,wsocketbuf)
+
+	unixCon.Close()
+}
+
+//func onMessageReceived(conn *net.UnixConn) {
+//	//for { // io Read will wait here, we don't need for loop to check
+//	// Read information from response
+//	data, err := parseResponse(conn)
+//	if err != nil {
+//		fmt.Println(err)
+//	} else {
+//		fmt.Printf("%v\tReceived from server: %s\n", time.Now(), string(data))
+//	}
+//
+//	// Exit when receive data from server
+//	exitSemaphore <- true
+//	//}
+//}
